5801e53b7f2b7c3fd66cbf253c2363787aaa599c
Sink <arseniiminiasy@gmail.com>
2019-08-22 12:47:12 +0300

    First commit

---
+++ vue.config.js
@@ -0,0 +1,20 @@
+const StyleLintPlugin = require('stylelint-webpack-plugin');
+
+module.exports = {
+  configureWebpack: {
+    plugins: [
+      new StyleLintPlugin({
+        files: ['**/*.{vue,htm,html,css,sss,less,scss,sass}'],
+      }),
+    ],
+  },
+  chainWebpack: (config) => {
+    const svgRule = config.module.rule('svg');
+
+    svgRule.uses.clear();
+
+    svgRule
+      .use('vue-svg-loader')
+      .loader('vue-svg-loader');
+  },
+};
