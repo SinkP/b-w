b814b4b9954f49e79c4dc4b95face74802e57219
Виталий <vitalij.baev.98@bk.ru>
2019-08-28 14:45:05 +0300

    Сделал главную страницу

---
+++ components/themain/AppHeader.vue
@@ -0,0 +1,81 @@
+<template>
+  <header class="header">
+    <div class="container">
+      <div class="header__inner">
+        <h1 class="header__title">
+          Всё, что <br>
+          тебе нужно <br>
+          В одном <br>
+          месте <br>
+        </h1>
+        <h2 class="header__subtitle">
+          Прогрессивная веб-студия
+        </h2>
+        <div class="header__logo">
+          <a href="#">B&amp;W</a>   
+        </div>
+        <div class="header__arrow">
+          <img src="../../static/Logotyps/header__arrow.svg" alt="arrow" />
+        </div>
+      </div>
+    </div>
+  </header>
+</template>
+
+<style scoped lang='sass'>
+
+  .header
+    padding-top: 36px
+    padding-bottom: 25px
+    background-image: url("../../static/themain/bg_header.png")
+    width: 100%
+    background-repeat: no-repeat
+    background-size: cover
+    position: relative
+    z-index: 0
+    &:after
+        content: ''
+        display: block
+        position: absolute
+        left: 0
+        top: 0
+        right: 0
+        bottom: 0
+        background-color: rgba(#000, 0.57)
+        z-index: -1
+      
+
+
+  .header__title
+    color: #ffffff
+    font-size: 48px
+    font-weight: 100
+    line-height: 48px
+    font-family: Roboto
+    margin-bottom: 14px
+
+  .header__subtitle
+    color: #ffffff
+    font-family: Roboto
+    font-size: 20px
+    font-weight: 100
+    line-height: 24px
+    margin-bottom: 10px
+    opacity: 0.7
+  
+  .header__logo
+    text-align: center
+    margin-bottom: 35px;
+    a
+      text-transform: uppercase
+      font-size: 20px
+      color: #171717
+      padding: 10px 15px
+      background-color: #ffffff
+      font-family: Roboto
+      font-weight: 100
+  
+  .header__arrow
+    text-align: center
+    
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,63 +1,160 @@
 <template>
-  <section class="container">
-    <div>
-      <app-logo/>
-      <h1 class="title">
-        web-studio
-      </h1>
-      <h2 class="subtitle">
-        Progressive web-studio B&W
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
+  <section class="black">
+    <app-header></app-header>
+
+    <div class="spickDo">
+      <div class="container">
+        <h2 class="spickDo__title">
+          Меньше слов<br />
+          Больше дела
+        </h2>
       </div>
     </div>
+
+    <div class="content">
+      <div class="container">
+        <div class="content__inner">
+          <div class="content__title">Сайт</div>
+          <div class="content__subtitle">Создание сайта любой сложности</div>
+          <div class="content__btn">
+            <a href="#">Подробнее</a>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="content">
+      <div class="container">
+        <div class="content__inner">
+          <div class="content__title">Дизайн</div>
+          <div class="content__subtitle">Создание сайта любой сложности</div>
+          <div class="content__btn">
+            <a href="#">Подробнее</a>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="content">
+      <div class="container">
+        <div class="content__inner">
+          <div class="content__title">Логотип</div>
+          <div class="content__subtitle">Создание сайта любой сложности</div>
+          <div class="content__btn">
+            <a href="#">Подробнее</a>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <footer class = "footer">
+      <div class="container">
+        <div class="footer__inner">
+          <div class="footer__title">
+            Немного о нас
+          </div>
+          <div class="footer__subtitle">
+            Собралась как-то команда<br>
+            профессионалов<br>
+            перфекционистов, <br>
+            уставшая от дерьмовых<br>
+            исполнителей заказов и<br>
+            основала эту студию<br>
+            Конец :D
+          </div>
+        </div>
+      </div>
+    </footer>
   </section>
 </template>
 
 <script>
+import appHeader from "../components/themain/AppHeader.vue";
 
 export default {
   components: {
+    appHeader
   }
-}
+};
 </script>
 
-<style>
-.container {
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
+<style scoped lang="sass" >
 
-.title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
+  .spickDo
+    background-color: #000
+    padding-top: 25px
+    border-bottom: 1px solid  #646464
 
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
+  .spickDo__title
+    color: #ffffff
+    font-family: Roboto
+    font-size: 30px
+    font-weight: 100
+    line-height: 30px
+    text-align: center
+    padding-bottom: 20px
 
-.links {
-  padding-top: 15px;
-}
+  .content
+    text-align: center
+   
+    padding-top: 20px
+    padding-bottom: 20px
+    background-color: #000
+    border-bottom: 1px solid  #646464
+
+  .content__title
+    color: #ffffff
+    font-family: Roboto
+    font-size: 20px
+    font-weight: 100
+    line-height: 24px
+    margin-bottom: 15px
+
+  .content__subtitle
+    color: #ffffff
+    font-family: Roboto
+    font-size: 16px
+    font-weight: 100
+    line-height: 24px
+
+  .content__btn
+    a
+      display: inline-block
+      color: #ffffff
+      font-family: Roboto
+      font-size: 16px
+      font-weight: 100
+      line-height: 24px
+      padding: 8px 70px
+      border: 1px solid #fff
+      margin-top: 10px
+      transition: background-color .2s linear, 
+      border .2s linear
+      &:hover
+           background-color: #3b2828
+           border-color: #624b4b
+
+  .footer
+    background-color: #000
+    text-align: center    
+    padding-top: 25px
+    padding-bottom: 45px
+
+
+  .footer__title
+    color: #ffffff
+    font-family: Roboto
+    font-size: 30px
+    font-weight: 100
+    line-height: 30px
+    margin-bottom: 28px
+
+  .footer__subtitle
+    margin-top: 
+    color: #ffffff
+    font-family: Roboto
+    font-size: 20px
+    font-weight: 100
+    line-height: 22px
 </style>
 
